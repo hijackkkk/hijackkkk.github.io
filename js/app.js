65e406f97a8f678e6e7f929fab88e6cb0ef3a05c
hijackkkk <1024592756@qq.com>
2018-04-07 17:06:52 +0800

    上传

---
+++ js/app.js
@@ -0,0 +1,73 @@
+window.onload=function () {
+    imgLocation("container","box")
+    var imgData={"data":[{"src":"1.jpg"},{"src":"2.jpg"},{"src":"3.jpg"},{"src":"4.jpg"},{"src":"5.jpg"},{"src":"6.jpg"},{"src":"7.jpeg"},{"src":"8.png"},{"src":"9.jpg"}]}
+    window.onscroll=function () {
+        if(checkFlag()){
+            var cparent=document.getElementById("container");
+            for(var i=0;i<imgData.data.length;i++){
+                var ccontent=document.createElement("div");
+                ccontent.className="box";
+                cparent.appendChild(ccontent);
+                var boximg=document.createElement("div");
+                boximg.className="box_img";
+                ccontent.appendChild(boximg);
+                var img=document.createElement("img");
+                img.src="img/"+imgData.data[i].src;
+                boximg.appendChild(img);
+            }
+            imgLocation("container","box")
+        }
+    }
+}
+
+function checkFlag() {
+    var cparent=document.getElementById("container");
+    var ccontent=getChildElement(cparent,"box");
+    var lastContentHeight=ccontent[ccontent.length-1].offsetTop;
+    var scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
+    var pageHeight=document.documentElement.clientHeight||document.body.clientHeight;
+    if(lastContentHeight<scrollTop+pageHeight){
+        return true;
+    }
+}
+function imgLocation(parent,content) {
+    var cparent=document.getElementById(parent);
+    var ccontent=getChildElement(cparent,content);
+    var imgWidth=ccontent[0].offsetWidth;
+    var num=Math.floor(document.documentElement.clientWidth / imgWidth);
+    cparent.style.cssText="width:"+imgWidth*num+"px;margin:0 auto";
+
+    var BoxHeightArr=[];
+    for(var i=0;i<ccontent.length;i++){
+        if(i<num){
+            BoxHeightArr[i]=ccontent[i].offsetHeight;
+        }
+        else{
+            var minheight=Math.min.apply(null,BoxHeightArr);    //引用数学函数找到最小高度的图片
+            var minIndex=getminheightLocation(BoxHeightArr,minheight);
+            ccontent[i].style.position="absolute";
+            ccontent[i].style.top=minheight+"px";
+            ccontent[i].style.left=ccontent[minIndex].offsetLeft+"px";
+            BoxHeightArr[minIndex]=BoxHeightArr[minIndex]+ccontent[i].offsetHeight;
+        }
+    }
+}
+//创建函数通过遍历获取高度最小图片位置
+function getminheightLocation(BoxHeightArr,minHeight) {
+    for(var i in BoxHeightArr){
+        if(BoxHeightArr[i]==minHeight){
+            return i;
+        }
+    }
+}
+
+function getChildElement(parent,content) {
+    var contentArr=[];
+    var allcontent=parent.getElementsByTagName("*");
+    for(var i=0;i<allcontent.length;i++){
+        if(allcontent[i].className==content){
+            contentArr.push(allcontent[i]);
+        }
+    }
+    return contentArr;
+}
